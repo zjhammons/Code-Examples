7218b8634f6ae0af70cc5463d291f92e709d8b2e
zjhammons <31661650+zjhammons@users.noreply.github.com>
2020-01-29 21:37:55 -0500

    Delete player.py

--- game/player.py
+++
@@ -1,9 +0,0 @@
-class player():
-    def playerScore(self,score):
-        score=score+1
-        return score
-
-    def playerMousePosition(self,xpos,ypos):
-        mouse_position=(xpos,ypos)
-
-
